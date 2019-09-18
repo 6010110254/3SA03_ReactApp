d42c9e75fea568d0fe7d244581a6d02723f32896
6010110254 <6010110254@psu.ac.th>
2019-09-18 19:34:56 +0700

    11 commit

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
         <WordCard value= "6010110254"/>
           <h2 > 3SA03</h2>
   
-          <h3 > Fadel hayeemareh 6010110254 Setion:01 </h3>
+          <h3 > Fadel hayeemareh 6010110254 Section:01 </h3>
          <h2 > CopyRight 2019 - React</h2>
          <br></br>
          <br></br>
